f161a52a8ca66c3a2f88a1052ff921e8daf29599
d.d.kozlov <kozlovdd2@gmail.com>
2024-07-19 19:01:00 +0500

    update design and main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 
 from PyQt5 import QtCore, QtWidgets, uic
 import sys
+import easygui
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -9,24 +10,25 @@ class MainWindow(QtWidgets.QMainWindow):
         uic.loadUi('Main.ui', self)
         self.date = self.findChild(QtWidgets.QTextBrowser, 'date')
         self.open = self.findChild(QtWidgets.QPushButton, 'open_button')
+        self.calculate = self.findChild(QtWidgets.QPushButton, 'save_button')
         self.date.setText(str(datetime.date.today()))
         self.show()
         self.open.clicked.connect(self.showDialog)
-    def showDialog(self):
-        options = QFileDialog.Options()
-        options = QFileDialog.ReadOnly
-        fileName, _ = QFileDialog.getOpenFileName(self, "Выбрать файл", "", "Все файлы (*);;Python файлы (*.py)", options=options)
-        if fileName:
-            print(f'Выбранный файл: {fileName}')
+        self.calculate.clicked.connect(self.doCalculation)
 
-    def openFile(self):
-        print("test")
-        options = QFileDialog.Options()
-        options = QFileDialog.ReadOnly
-        self.file = QFileDialog.getOpenFileName(self, "Open File", options=options)
-        self.filename = self.findChild(QtWidgets.QTextBrowser, 'file_name')
-        self.filename.setText(str(self.file[0]))
-        #func for open file
+    def showDialog(self):
+        input_file = easygui.fileopenbox(filetypes=["*.docx"])
+        self.file = self.findChild(QtWidgets.QTextBrowser, 'file_name')
+        self.file.setPlainText(input_file)
+
+    def doCalculation(self):
+        if self.file.toPlainText() != None:
+            print(input_file)
+            self.table = f.open(self.file)
+        else:
+            print("Error")
+        print(300)
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
