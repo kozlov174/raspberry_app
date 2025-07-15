e3d2c5e153bc649ba610a155b555e2c6640ace1e
d.d.kozlov <kozlovdd2@gmail.com>
2025-07-15 16:31:26 +0500

    Update GPIO Thread

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-xinput set-prop 7 "Coordinate Transformation Matrix" 0 -1 -1 1 0 0 0 0 1
+xinput set-prop 6 "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
 cd /root/raspberry_app/
 python3 main.py
